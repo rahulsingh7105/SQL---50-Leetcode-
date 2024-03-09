b46f192f6b5727cd8c91186f649c6ade602c735b
Rahul Singh <rahulsingh71059@gmail.com>
2024-03-10 00:57:44 +0530

    Update and rename 1148. Article Views I.sql to Select/1148. Article Views I.sql

--- Select/1148. Article Views I.sql
+++ Select/1148. Article Views I.sql
@@ -1,6 +1,7 @@
 /*
 https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
 */
+
 # Write your MySQL query statement below
 SELECT DISTINCT author_id AS id 
 FROM Views 
