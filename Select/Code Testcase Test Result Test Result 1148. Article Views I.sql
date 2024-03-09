9d64ef79f80114b04e9cc4948703c31ee47249c7
Rahul Singh <rahulsingh71059@gmail.com>
2024-03-10 00:55:39 +0530

    Create  Code Testcase Test Result Test Result 1148. Article Views I.sql

---
+++ Select/Code Testcase Test Result Test Result 1148. Article Views I.sql
@@ -0,0 +1,8 @@
+/*
+https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
+*/
+# Write your MySQL query statement below
+SELECT DISTINCT author_id AS id 
+FROM Views 
+WHERE author_id = viewer_id 
+ORDER BY id
